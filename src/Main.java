04d91978b474506805656353945a64ee1ceff8b0
Dawe129 <dpivonka1@gmail.com>
2024-05-23 12:00:51 +0200

    Vypis + prepsani souboru na .csv

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
-import javax.swing.event.ListDataEvent;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         String slozka = "C:\\Users\\dpivo\\Documents\\GitHub\\Zadani_2\\geo_coded_DM_jaro_2024.csv";
         List<String[]> data = CteniCSV.CSV(slozka);
@@ -10,9 +9,18 @@ public class Main {
         String csvObyvatel = "C:\\Users\\dpivo\\Documents\\GitHub\\Zadani_2\\Obyvatele";
         List<String[]> data1 = Obyvatele.ObyvateleCSV(csvObyvatel);
 
-
+        /*
         CteniCSV.odebraniCizichZemi(data);
         for (String[] radek : data) {
+            for (String hodnota : radek) {
+                System.out.print(hodnota + " ");
+            }
+            System.out.println();
+        }
+        */
+
+        Obyvatele.ObyvateleCSV(csvObyvatel);
+        for (String[] radek : data1) {
             for (String hodnota : radek) {
                 System.out.print(hodnota + " ");
             }
